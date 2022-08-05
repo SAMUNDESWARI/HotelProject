69d170a4a850f58fd481881b84ff333354fea2d5
koushik <koushikmamilla123@gmail.com>
2022-08-05 14:51:49 +0530

    Add project files.

---
+++ hotelRepository/hotelRepository.cs
@@ -0,0 +1,13 @@
+﻿using hotelRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotelRepository
+{
+   public class hotelRepository : IhotelRepository
+    {
+    }
+}
